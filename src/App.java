aa4b7cceca34ecc15e69698266600a9d054173e7
bruna <brunaruback07@gmail.com>
2023-03-03 10:01:00 -0300

    First class: position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import boardgame.Position;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        Position p = new Position(5, 3);
+        System.out.println(p);
     }
 }
